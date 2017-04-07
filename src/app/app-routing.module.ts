2c7da84a57a3813d88010254c0e8f7ed051afbad
= <karfannacif@gmail.com>
2017-04-06 23:51:54 -0300

    Added search artist by name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomepageComponent } from './homepage/homepage.component';
-import { NewsComponent } from './news/news.component';
-import { HotspotsComponent } from './hotspots/hotspots.component';
+import { NewsListComponent } from './news/news-list.component';
+import { HotspotsListComponent } from './hotspots/hotspots-list.component';
+import { ArtistsComponent } from "./artists/artists.component";
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomepageComponent },
-    { path: 'news', component: NewsComponent },
-    { path: 'hotspot', component: HotspotsComponent }
+    { path: 'news', component: NewsListComponent },
+    { path: 'hotspot', component: HotspotsListComponent },
+    { path: 'artists', component: ArtistsComponent }
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { NewsComponent } from './news/news.component';
 import { NewsListComponent } from './news/news-list.component';
 import { NewsService } from './news/news.service';
-import { HotspotsComponent } from './hotspots/hotspots.component';
 import { HotspotService } from './hotspots/hotspot.service';
 import { HotspotsListComponent } from './hotspots/hotspots-list.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { SearchArtistService } from "./artists/search-artist.service";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomepageComponent,
-    NewsComponent,
     NewsListComponent,
-    HotspotsComponent,
-    HotspotsListComponent
+    HotspotsListComponent,
+    ArtistsComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +33,7 @@ import { HotspotsListComponent } from './hotspots/hotspots-list.component';
     NgbModule.forRoot(),
     AppRoutingModule
   ],
-  providers: [NewsService, HotspotService],
+  providers: [NewsService, HotspotService, SearchArtistService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
